9bca98e7ae63e9ffabd1d7522b710ca0d7de7336
NightPixel24 <52042379+NightPixel24@users.noreply.github.com>
2021-10-17 03:03:53 +0800

    Working
    side bar works with pages, Needs fixing all pages are sideways

--- src/pages/ReportPage.js
+++ src/pages/ReportPage.js
@@ -1,27 +1,27 @@
-import { Button, TextField } from "@material-ui/core";
+import React from 'react';
+import {Button, TextField } from "@material-ui/core";
 
-export function ReportPage() {
-    return(
-        <div>
+const ReportPage = () => {
+  return (
+    <div className='report'>
 
-            <header> Report a Sighting </header>
-            
-            <form class="report-form">
-                <TextField id="nameInput" label="Name"></TextField>
-                <br/>
-                <TextField id="locationInput" label="Location"></TextField>
-                <br/>
-                <TextField id="idInput" label="Identification"></TextField>
-                <br/>
-                <TextField multiline id="descriptionInput" label="Description"></TextField>
-            </form>
+        <header> Report a Sighting </header>
 
-            <Button variant="outline" id="submitButton" onClickSubmit={ (event) => { onClickSubmit() } }>submit</Button>
+        <form class="report-form">
+            <TextField id="nameInput" label="Name"></TextField>
+            <br/>
+            <TextField id="locationInput" label="Location"></TextField>
+            <br/>
+            <TextField id="idInput" label="Identification"></TextField>
+            <br/>
+            <TextField multiline id="descriptionInput" label="Description"></TextField>
+        </form>
 
-        </div>
-    );
-}
+        <Button variant="outline" id="submitButton" onClick={ (event) => {} }>submit</Button>
 
-function onClickSubmit(event) {
+    </div>
+  );
+};
 
-}
+export default ReportPage;
+
