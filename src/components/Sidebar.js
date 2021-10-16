5183d9b92454ba83840042df0dc4cdcbcc668a83
Kaiyuso <kysoh01@gmail.com>
2021-10-17 04:02:29 +0800

    Top bar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,6 +6,7 @@ import * as AiIcons from 'react-icons/ai';
 import { SidebarData } from './SidebarData';
 import SubMenu from './SubMenu';
 import { IconContext } from 'react-icons/lib';
+import './topBar.css'
 
 const Nav = styled.div`
   background: #15171c;
@@ -41,10 +42,14 @@ const SidebarWrap = styled.div`
   width: 100%;
 `;
 
+var date, time; 
+
 const Sidebar = () => {
   const [sidebar, setSidebar] = useState(false);
 
   const showSidebar = () => setSidebar(!sidebar);
+
+  setInterval(doDate, 1000)
 
   return (
     <>
@@ -54,6 +59,10 @@ const Sidebar = () => {
             <FaIcons.FaBars onClick={showSidebar} />
           </NavIcon>
         </Nav>
+        <image className='icon'></image>
+        <h1 className='title'>Copium</h1>
+        <h1 className='time' id='timeID'> </h1>
+        <h1 className='time' id='dateID'> </h1>
         <SidebarNav sidebar={sidebar}>
           <SidebarWrap>
             <NavIcon to='#'>
@@ -70,3 +79,16 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
+
+function doDate() {
+  var day = new Date();
+  date = day.getDate() + '/' + (day.getMonth() + 1) + '/' + day.getFullYear();
+  time = formatDate(day.getHours()) + ':' + formatDate(day.getMinutes()) + ':' + formatDate(day.getSeconds());
+
+  document.getElementById("dateID").textContent="Date: " + date;
+  document.getElementById("timeID").textContent="Time: " + time;
+}
+
+function formatDate(number) {
+  return String(number).padStart(2, '0');
+}
