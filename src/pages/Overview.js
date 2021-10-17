ba74b4f9e18050c8a0883eb30b0e8fce44527405
NightPixel24 <joshua.rangan007@gmail.com>
2021-10-17 14:18:57 +0800

    Update color
    color and spacing

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -4,10 +4,16 @@ const Overview = () => {
   return (
     <div className='home'>
       <h1>Welcome to Copium</h1>
+
+      <br></br>
+
       <h3>About Us</h3>
       <p>We at Copium wish to provide you the user with the latest information on all Alien Activity</p>
       <p>This website has been dedicated to all extra terrestrial activity and the safe practices interacting with them</p>
       <p>Our primary purpose here at Copium is the Protection and Safety of all humanity</p>
+
+      <br></br>
+      
       <p>Thank You, CEO (Guang Yu)</p>
     </div>
   );
