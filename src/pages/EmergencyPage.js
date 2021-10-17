f404996d15947a6225d9aa026efee704aae399da
Autobot76 <49295033+Autobot76@users.noreply.github.com>
2021-10-17 16:06:45 +0800

    Added description to each contact.

--- src/pages/EmergencyPage.js
+++ src/pages/EmergencyPage.js
@@ -22,15 +22,25 @@ const EmergencyPage = () => {
             <p>
                 '000'
             </p>
+            <p>
+                Please contact for small alien sightings. Listen carefully to provided instructions<br></br>
+                and report back to your responding officer if symptoms develop or persist.
+            </p>
 
             <p></p>
             <br></br>
 
             <h3>
-                Alien Task Force
+                Alien Task Force (ATF)
             </h3>
             <p>
                 '779-555'
+            </p>
+            <p>
+                Please contact for medium to large alien sightings. Listen carefully to provided instructions<br></br>
+                and report back to your responding officer if symptoms persist or develop. You will be required <br></br>
+                to attend a weekly doctors appointment.<br></br>
+                In case of hostile alien behaviour please contact this line. Full force will be used if necessary.
             </p>
             
             <p></p>
@@ -42,6 +52,12 @@ const EmergencyPage = () => {
             <p>
                 '223-555-7832'
             </p>
+            <p>
+                Please contact for non-hostile small to medium alien sightings. Listen carefully to provided instructions.<br></br>
+                DO NOT touch alien materials you may have encountered.<br></br>
+                DO report back to your responding officer if symptoms persist or develop. You will be required <br></br>
+                to attend a weekly doctors appointment.<br></br>
+            </p>
 
             <p></p>
             <br></br>
@@ -51,6 +67,12 @@ const EmergencyPage = () => {
             </h3>
             <p>
                 '81-80-553'
+            </p>
+            <p>
+                Please contact for non-hostile medium to large alien sightings. Listen carefully to provided instructions.<br></br>
+                DO NOT touch alien materials you may have encountered.<br></br>
+                DO report back to your responding officer if symptoms persist or develop. You will be required <br></br>
+                to attend a weekly doctors appointment.<br></br>
             </p>
         </div>
 
