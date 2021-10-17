3eebfd95765bed23ab28d956c95d35bb3ba4c5c0
NightPixel24 <joshua.rangan007@gmail.com>
2021-10-17 14:06:20 +0800

    Home Page
    added description

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Overview = () => {
   return (
     <div className='home'>
-      <h1>Copium</h1>
+      <h1>Welcome to Copium</h1>
+      <h3>About Us</h3>
+      <p>We at Copium wish to provide you the user with the latest information on all Alien Activity</p>
+      <p>This website has been dedicated to all extra terrestrial activity and the safe practices interacting with them</p>
+      <p>Our primary purpose here at Copium is the Protection and Safety of all humanity</p>
+      <p>Thank You, CEO (Guang Yu)</p>
     </div>
   );
 };
